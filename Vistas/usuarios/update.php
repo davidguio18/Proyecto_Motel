35340c9d093134542a486f8857d36744c877cbe1
jcHernandezB <44384488+jcHernandezB@users.noreply.github.com>
2018-11-15 16:25:58 -0500

    Interfaz Usuarios
    Se añade intefaz a las funciones de crear, actualizar y administrar usuarios

--- Vistas/usuarios/update.php
+++ Vistas/usuarios/update.php
@@ -1,30 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta charset="utf-8">
-	<title>Actualizar usuario</title>
-</head>
-<body>
- <h1>Actualizar <?= $usuario->nombres ?></h1>
- <form action="" method="post" autocomplete="off">
- 	<label>Nombres</label>
- 	<input type="text" name="Usuarios[nombres]" value="<?= $usuario->nombres ?>" required />
- 	<label>Apellidos</label>
- 	<input type="text" name="Usuarios[apellidos]" value="<?= $usuario->apellidos ?>" required />
- 	<label>Estado:</label>
- 	<select name="Usuarios[perfil]" required>
- 		<option value="Administrador" <?= ($usuario->perfil=="Administrador") ? 'selected="selected"' : '' ?>>Administrador</option>
- 		<option value="Vendedor"<?= ($usuario->perfil=="Vendedor") ? 'selected="selected"' : '' ?>>Vendedor</option>
- 	</select>
- 	<label>Documento</label>
- 	<input type="text" required name="Usuarios[documento]" value="<?= $usuario->documento ?>" onkeypress="return valida(event)"/>
- 		<label>Contrasena</label>
- 	<input type="password" name="Usuarios[contrasena]" value="<?= $usuario->contrasena ?>"/>
+<?php include_once ("Vistas/cabecera.php"); ?>
 
- 	
- 	<button type="submit">Guardar cambios</button>
- </form>
-</body>
+<div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                
+                                <div class="card">
+                                    <div class="card-header">
+                                        <strong>Actualizar</strong> Usuario  <?= $usuario->nombres ?>
+                                    </div>
+                                    <div class="card-body card-block">
+                                        <form action="" method="post" class="" autocomplete="off">
+                                            <div class="form-group">
+                                                <label for="nf-email" class=" form-control-label">Nombres</label>
+                                                <input type="text" id="nf-email" name= "Usuarios[nombres]" placeholder="Ingrese Nombre de Usuario.." class="form-control"  value="<?= $usuario->nombres ?>" >
+                                            </div>
+                                             <div class="form-group">
+                                                <label for="nf-email" class=" form-control-label">Apellidos</label>
+                                                <input type="text" id="nf-email" name="Usuarios[apellidos]" placeholder="Ingrese Marca de Producto.." class="form-control"  value="<?= $usuario->apellidos ?>" >
+                                            </div>
+                                             <div class="form-group">
+                                                <label for="nf-email" class=" form-control-label">Tipo de Usuario</label>
+                                               <select name="Usuarios[perfil]" required>
+                                                     <option value="Administrador" <?= ($usuario->perfil=="Administrador") ? 'selected="selected"' : '' ?>> Administrador </option>
+                                                      <option value="Vendedor"<?= ($usuario->perfil=="Vendedor") ? 'selected="selected"' : '' ?>> Vendedor </option>
+                                               </select>
+                                            </div>
+                                             <div class="form-group">
+                                                <label for="nf-email" class=" form-control-label">Documento</label>
+                                                <input type="number" id="nf-email" placeholder="Ingrese Numero de Documento.." class="form-control"  name="Usuarios[documento]" onkeypress="return valida(event)"  value="<?= $usuario->documento ?>">
+                                            </div>
+
+                                             <div class="form-group">
+                                                <label for="nf-email" class=" form-control-label">Contraseña</label>
+                                                <input type="password" id="nf-email" name="Usuarios[contrasena]" placeholder="Ingrese Marca de Producto.." class="form-control" value="<?= $usuario->contrasena ?>" >
+                                            </div>
+
+                                             <div class="card-footer">
+                                                 <button type="submit" class="btn btn-primary btn-sm">
+                                                    <i class="fa fa-dot-circle-o"></i> Actualizar
+                                                </button>
+                                                
+                                    </div>
+                                         
+                                        </form>
 <script>
 function valida(e){
     tecla = (document.all) ? e.keyCode : e.which;
@@ -40,4 +60,14 @@ function valida(e){
     return patron.test(tecla_final);
 }
 </script>
-</html>
+                                    </div>
+                                    
+                                </div>
+                               
+                                
+                                </div>
+                            </div>
+
+                            </div>
+                        </div>
+ <?php require_once("Vistas/footer.php"); ?>
