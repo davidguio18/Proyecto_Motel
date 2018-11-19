4e97b97caad97aca0eeb0acb60487df24d2224f0
jcHernandezB <44384488+jcHernandezB@users.noreply.github.com>
2018-11-19 12:46:29 -0500

    Correcion Errores Carga de Interfaces
    Solución de Carga de Interfaces, eliminando archivo htacces de recursos de interfaces

--- Vistas/home/home.php
+++ Vistas/home/home.php
@@ -2,6 +2,16 @@
 
             <!-- MAIN CONTENT-->
             <div class="main-content">
+
+            	     <h3 align = "center">Usted Ha Ingresado Como Administrador</h3>
+            	     <br>
                  <h1 align="center">Para Iniciar Seleccione Una Opci&oacute;n Del Men&uacute;</h1>
+
+                 	<br>
+
+               
+            
+
+               
             </div>
 <?php include_once("Vistas/footer.php"); ?>
