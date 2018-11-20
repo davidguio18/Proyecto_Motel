a36051025513561e0a06899ece89819c5f5269fe
adrissgoye <adrianaluciagoye@gmail.com>
2018-11-19 23:32:44 -0500

    interfaz admin

---
+++ Vistas/home/homeus.php
@@ -0,0 +1,10 @@
+<?php include_once("Vistas/cabeceraus.php"); ?>
+
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+            <h1>Administracion</h1>
+                               
+
+
+            </div>
+<?php include_once("Vistas/footer.php"); ?>
